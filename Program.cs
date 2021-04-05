2183d239d8868e032c934b216ffc19334f82c171
jiang <2249972133@qq.com>
2021-04-05 16:57:08 +0800

    test push webhook data

--- Program.cs
+++ Program.cs
@@ -78,7 +78,7 @@ namespace VistorSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
             Client c = new Client();
             c.AddToList(new Book(10));
             c.AddToList(new Article(30));
